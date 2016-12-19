5ddc728c4010ab57f3e9aa6f5afe4c7a3b1d0515
Alade of GOD <michealakintola106@yahoo.coom>
2016-12-19 14:02:11 -0800

    re-designed createloginactivity from image login to pin login

--- app/src/main/java/com/studio/pogworld/dropdream/CreateLoginActivity.java
+++ app/src/main/java/com/studio/pogworld/dropdream/CreateLoginActivity.java
@@ -1,96 +1,122 @@
 package com.studio.pogworld.dropdream;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * Created by Micheal on 12/16/2016.
  */
 
-public class CreateLoginActivity extends AppCompatActivity {
+public class CreateLoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     //SharedPreferences someData;
-    ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16;
-    Button btnCreate;
-    GridView myShow;
-    public static String myImgArray = "";
-
-    private void GetImages(){
-        img1 = (ImageView)  findViewById(R.id.iv1);
-        img2 = (ImageView)  findViewById(R.id.iv2);
-        img3 = (ImageView)  findViewById(R.id.iv3);
-        img4 = (ImageView)  findViewById(R.id.iv4);
-        img5 = (ImageView)  findViewById(R.id.iv5);
-        img6 = (ImageView)  findViewById(R.id.iv6);
-        img7 = (ImageView)  findViewById(R.id.iv7);
-        img8 = (ImageView)  findViewById(R.id.iv8);
-        img9 = (ImageView)  findViewById(R.id.iv9);
-        img10 = (ImageView)  findViewById(R.id.iv10);
-        img11 = (ImageView)  findViewById(R.id.iv11);
-        img12 = (ImageView)  findViewById(R.id.iv12);
-        img13 = (ImageView)  findViewById(R.id.iv13);
-        img14 = (ImageView)  findViewById(R.id.iv14);
-        img15 = (ImageView)  findViewById(R.id.iv15);
-        img16 = (ImageView)  findViewById(R.id.iv16);
-        btnCreate = (Button) findViewById(R.id.btnCreate);
-        myShow = (GridView) findViewById(R.id.glShow);
-
+    Button btnCreate, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
+    ImageButton btnDel;
+    EditText etPin;
+    private void getButton(){
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn0 = (Button) findViewById(R.id.btn0);
+        btnDel = (ImageButton) findViewById(R.id.btnDel);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnDel.setOnClickListener(this);
+        etPin = (EditText) findViewById(R.id.etPin);
     }
     @Override
     protected void onCreate(Bundle savedIntanceState){
         super.onCreate(savedIntanceState);
         setContentView(R.layout.create_pattern);
-        GetImages();
-
-        //someData = getSharedPreferences(myImgArray, 0);
-        GetImages();
-        //listening to onclicklistener
-        img1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myImgArray = myImgArray+"A";
-                myShow.addView(img1);
-            }
-        });
-        img2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myImgArray = myImgArray+"B";
-                myShow.addView(img2);
-            }
-        });
-        img3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myImgArray = myImgArray+"B";
-                myShow.addView(img3);
-            }
-        });
-
+        getButton();
+        btnCreate = (Button) findViewById(R.id.btnCreate);
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (myImgArray.equals("")) {
-                    btnCreate.setVisibility(view.GONE);
-                    TextView somenote = (TextView) findViewById(R.id.somenote);
-                    somenote.setVisibility(view.GONE);
-                    TextView imageremind = (TextView) findViewById(R.id.imageremind);
-                    imageremind.setVisibility(view.VISIBLE);
-                    LinearLayout optionLay = (LinearLayout) findViewById(R.id.options);
-                    optionLay.setVisibility(view.VISIBLE);
-
-                }
-                else Toast.makeText(CreateLoginActivity.this, "Please click some images before proceeding", Toast.LENGTH_SHORT).show();
-
+                fireCustomDialog();
+                //Toast.makeText(CreateLoginActivity.this, "this is your pin: "+etPin, Toast.LENGTH_LONG).show();
             }
         });
+
     }
+
+    private void setPin (Button btn, EditText eT){
+        eT.setText(eT.getText().toString()+btn.getText().toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn0:
+                setPin(btn0, etPin);
+                break;
+            case R.id.btn1:
+                setPin(btn2, etPin);
+                break;
+            case R.id.btn2:
+                setPin(btn2, etPin);
+                break;
+            case R.id.btn3:
+                setPin(btn3, etPin);
+                break;
+            case R.id.btn4:
+                setPin(btn4, etPin);
+                break;
+            case R.id.btn5:
+                setPin(btn5, etPin);
+                break;
+            case R.id.btn6:
+                setPin(btn6, etPin);
+                break;
+            case R.id.btn7:
+                setPin(btn7, etPin);
+                break;
+            case R.id.btn8:
+                setPin(btn8, etPin);
+                break;
+            case R.id.btn9:
+                setPin(btn9, etPin);
+                break;
+            case R.id.btnDel:
+                etPin.setText("");
+                break;
+        }
+    }
+
+    private void fireCustomDialog() {
+        new AlertDialog.Builder(CreateLoginActivity.this)
+                .setTitle("your login pin is: "+etPin.getText().toString())
+                .setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        CreateLoginActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        finish();
+                    }
+                })
+                .create();
+        }
 
 }
--- app/src/main/java/com/studio/pogworld/dropdream/LoginActivity.java
+++ app/src/main/java/com/studio/pogworld/dropdream/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 
 /**
  * Created by Micheal on 12/16/2016.
@@ -13,29 +12,10 @@ import android.widget.ImageView;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     //SharedPreferences someData;
-    ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16;
     public static String myImgLog = "";
     //list.add("1");
     //list.add("2");
     //list.add("3");
-    private void GetImages(){
-        img1 = (ImageView)  findViewById(R.id.iv1);
-        img2 = (ImageView)  findViewById(R.id.iv2);
-        img3 = (ImageView)  findViewById(R.id.iv3);
-        img4 = (ImageView)  findViewById(R.id.iv4);
-        img5 = (ImageView)  findViewById(R.id.iv5);
-        img6 = (ImageView)  findViewById(R.id.iv6);
-        img7 = (ImageView)  findViewById(R.id.iv7);
-        img8 = (ImageView)  findViewById(R.id.iv8);
-        img9 = (ImageView)  findViewById(R.id.iv9);
-        img10 = (ImageView)  findViewById(R.id.iv10);
-        img11 = (ImageView)  findViewById(R.id.iv11);
-        img12 = (ImageView)  findViewById(R.id.iv12);
-        img13 = (ImageView)  findViewById(R.id.iv13);
-        img14 = (ImageView)  findViewById(R.id.iv14);
-        img15 = (ImageView)  findViewById(R.id.iv15);
-        img16 = (ImageView)  findViewById(R.id.iv16);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,62 +40,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
 
         //someData = getSharedPreferences(myImgLog, 0);
-        GetImages();
-        //listening to onclicklistener
-        img1.setOnClickListener(this);
-        img2.setOnClickListener(this);
-        img3.setOnClickListener(this);
-        img4.setOnClickListener(this);
-        img5.setOnClickListener(this);
-        img6.setOnClickListener(this);
-        img7.setOnClickListener(this);
-        img8.setOnClickListener(this);
-        img9.setOnClickListener(this);
-        img10.setOnClickListener(this);
-        img11.setOnClickListener(this);
-        img12.setOnClickListener(this);
-        img13.setOnClickListener(this);
-        img14.setOnClickListener(this);
-        img15.setOnClickListener(this);
-        img16.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.iv1:
-                break;
-            case R.id.iv2:
-                break;
-            case R.id.iv3:
-                break;
-            case R.id.iv4:
-                break;
-            case R.id.iv5:
-                break;
-            case R.id.iv6:
-                break;
-            case R.id.iv7:
-                break;
-            case R.id.iv8:
-                break;
-            case R.id.iv9:
-                break;
-            case R.id.iv10:
-                break;
-            case R.id.iv11:
-                break;
-            case R.id.iv12:
-                break;
-            case R.id.iv13:
-                break;
-            case R.id.iv14:
-                break;
-            case R.id.iv15:
-                break;
-            case R.id.iv16:
-                break;
-
         }
 
     }
